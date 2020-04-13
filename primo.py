5ad6a79840941a49d573bd14cb65adb711290dd5
yumilr <julisa.lapa@utec.edu.pe>
2020-04-13 18:09:17 -0500

    tarea-3

---
+++ primo.py
@@ -0,0 +1,11 @@
+n=int(input('Ingrese número: '))
+if n==0:
+  print('El número no es admitido')
+else:
+  for i in range (2,n-1):
+    if n/i:
+      print('El número no es primo')
+      break
+    else:
+      print('El número no es primo')
+      break
